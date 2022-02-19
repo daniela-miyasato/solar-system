dc12bf3ce7b8bdb9eca8dbea693bfd58e6b59c53
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-02-18 21:11:18 -0300

    Requisito 8 - Missions component implementation

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        teste de retorno
+        <Title headline="Missões" />
       </div>
     );
   }
