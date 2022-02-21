9fe011a6a65414b5981d1a18381a0cdc50f3692a
Daniela Miyasato Goto <daniela.miyasato@live.com>
2022-02-21 18:45:36 -0300

    Requisito 10 - MissionCard component implementation

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
-        <Title headline="Missões" />
-      </div>
+      <>
+        <div data-testid="missions">
+          <Title headline="Missões" />
+        </div>
+        <div>
+          {missions.map((element) => (
+            <MissionCard
+              key={ element.name }
+              name={ element.name }
+              year={ element.year }
+              country={ element.country }
+              destination={ element.destination }
+            />
+          ))}
+        </div>
+      </>
     );
   }
 }
